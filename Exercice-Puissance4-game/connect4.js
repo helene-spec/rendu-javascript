1b3e948da4becb9624e5f12679c99f197502d3e1
helene-spec <kceehelene4@gmail.com>
2026-08-20 05:24:50 +0200

    mise à jour

---
+++ Exercice-Puissance4-game/connect4.js
@@ -0,0 +1,30 @@
+class Connect4 {
+    constructor(selector) {
+        this.ROWS = 6;
+        this.COLS = 7;
+        this.player = 'red';
+        this.selector = selector;
+        this.isGameOver = false;
+        this.onPlayerMove = function () { };
+        this.createGrid();
+        this.setupEventListeners();
+    }
+
+    createGrid() {
+        const $board = $(this.selector);
+        $board.empty();
+        this.isGameOver = false;
+        this.player = 'red';
+        for (let row = 0; row < this.ROWS; row++) {
+            const $row = $('<div>')
+                .addClass('row');
+            for (let col = 0; col < this.COLS; col++) {
+                const $col = $('<div>')
+                    .addClass('col empty')
+                    .attr('data-col', col)
+                    .attr('data-row', row);
+                $row.append($col);
+            }
+            $board.append($row);
+        }
+    }
--- Exercice-Puissance4-game/script.js
+++ Exercice-Puissance4-game/script.js
@@ -1,1 +1,2 @@
 $(document).ready(function () {
+    const connect4 = new Connect4('#connect4')
